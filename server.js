658cdd5e5ed5de435ff254b5db71adb69176c552
Rachael Shima <rachael_shima@hotmail.com>
2018-09-18 13:17:37 -0700

    Rachael Shima added an alert box function
    Created an alert box to welcome users to PawMD

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.welcome('/', function (name, res){
+  window.alert("Welcome to PawMD!");
+})
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
