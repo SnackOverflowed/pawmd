6353b5c262718f07832d009ac1676c87415fa663
Ryan Tang <phattang1226@gmail.com>
2018-11-20 13:18:41 -0800

    Worked on getBreedInfo from search page

--- src/main/resources/static/js/home-control.js
+++ src/main/resources/static/js/home-control.js
@@ -1,7 +1,15 @@
 
 var pawmdApp = angular.module('pawmdApp', []);
 
-pawmdApp.controller('HomePageCtrl', function ($scope, $http, $window) {
+pawmdApp.controller('HomePageCtrl', function ($scope, $http, $window, $rootScope) {
+
+  // $rootScope.$on("CallMy2ControllerMethod", function(event,param1){
+  //          $scope.My2ControllerMethod(param1);
+  // });
+ 
+  $scope.My2ControllerMethod = function(param1) {
+    alert(param1);
+  }
 
   $scope.getBreedInfo = function() {
 	  	$http.get("/home/" + $scope.breed_name)
@@ -14,4 +22,9 @@ pawmdApp.controller('HomePageCtrl', function ($scope, $http, $window) {
 	  	$window.location.href = '../search.html';
   }
 
+
+
+  function f1(breedName) {
+  	alert(breedName);
+  }
 });
--- src/main/resources/static/js/search-control.js
+++ src/main/resources/static/js/search-control.js
@@ -1,6 +1,8 @@
 var pawmdApp = angular.module('pawmdApp', []);
 
-pawmdApp.controller('SearchPageCtrl', function ($scope, $http, $window) {
+pawmdApp.controller('SearchPageCtrl', 
+
+	function ($scope, $http, $window, $rootScope) {
 
 	$scope.loadBreedList = function() {
 	  	$http.get("/search")
@@ -16,8 +18,40 @@ pawmdApp.controller('SearchPageCtrl', function ($scope, $http, $window) {
 		  		$scope.breedNameAndURL = data;
 	  	});
 	};
-	
-	
+
+	$scope.getInfoForDog = function(breed_name) {
+
+		$scope.f1(breed_name);
+		//$rootScope.$emit("CallMy2ControllerMethod", {message: 'in search'});
+		// console.log("in search " + breed_name);
+		// $scope.$broadcast('parentmethod', { message: "gell" });
+		//$window.location = '../home.html#searchInfo';
+	    
+	};
+
+
+	function searchDogsList()
+ 	{
+	    // Declare variables
+	    var input, filter, ul, li, a, i;
+	    input = document.getElementById('breedInput');
+	    filter = input.value.toUpperCase();
+	    ul = document.getElementById("myUL");
+	    li = ul.getElementsByTagName('li');
+
+	    // Loop through all list items, and hide those who don't match the search query
+	    for (i = 0; i < li.length; i++) {
+	        a = li[i].getElementsByTagName("a")[0];
+	        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+	            li[i].style.display = "";
+	        } else {
+	            li[i].style.display = "none";
+	        }
+	    }
+	}
+
 	$scope.loadBreedList();
 	
 });
+
+
