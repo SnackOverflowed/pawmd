8a5e7cdab5131976427813821a155fd5c15d043b
Kim Nguyen <ngankimnguyen589@yahoo.com>
2018-11-19 16:51:39 -0800

    kimN

--- src/main/resources/static/js/search-control.js
+++ src/main/resources/static/js/search-control.js
@@ -1,22 +1,7 @@
-
-var pawmdApp = angular.module('pawmdApp', []);
-
-pawmdApp.controller('SearchPageCtrl', function ($scope, $http, $window) {
-
-	$scope.loadBreedList = function() {
-	  	$http.get("/search")
-	  		.success(function(data){
-		  		$scope.breedList = data;
-	  	});
-	}
-
-	$scope.hoverIn = function(breed_name){
-	    
-	    $http.get("/breedImageURL/" + breed_name)
-	  		.success(function(data){
-		  		$scope.breedNameAndURL = data;
-	  	});
-	};
-
-  	$scope.loadBreedList();
-});
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+}
